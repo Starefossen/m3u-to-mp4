7b9f271afd549479daa6f1ee9f65e767ad6e6e2c
Hans Kristian Flaatten <hans.kristian.flaatten@turistforeningen.no>
2015-04-12 10:11:02 +0200

    Tag release v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # m3u8-to-mp3
+
 Bulk record m3u8 playlists to mp3 files using ffmpeg
+
+## Requirements
+
+* Docker
+* Docker Compose
+
+## Files
+
+Put you `m3u8` playlists in the `playlists` directory. Corresponding recordings
+will be avaiable in the `recordings` directory.
+
+```
+.
+|-- run.sh
+|-- playlists
+|   |-- master1.m3u8
+|   |-- master2.m3u8
+|   `-- master3.m3u8
+`-- recordings
+    |-- master1.mp4
+    |-- master2.mp4
+    `-- master3.mp4
+```
+
+## Usage
+
+```
+docker-compose run -rm record
+```
+
---
+++ run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+PLAYLISTS_DIR=./playlists
+RECORDINGS_DIR=./recordings
+
+function record {
+  local -r IN=$1
+  local -r OUT=$2
+
+  ffmpeg -re -i "${IN}" -c copy -bsf:a aac_adtstoasc "${OUT}"
+}
+
+for path in ${PLAYLISTS_DIR}/*; do
+  file=`basename $path`
+
+  m3u8_path="${path}"
+  mp4_path="${RECORDINGS_DIR}/${file%.*}.mp4"
+
+  if [ -f ${mp4_path} ]; then
+    echo "Skipping ${file}, already recorded!"
+  else
+    echo "${m3u8_path} > ${mp4_path}"
+    record "${m3u8_path}" "${mp4_path}"
+  fi
+
+  echo "Everything is done!"
+done
+
